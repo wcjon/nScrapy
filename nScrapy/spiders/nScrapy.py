5d948d62b8ae500c548a3b0f9bfac06763b0f124
wcjon <38473077+wcjon@users.noreply.github.com>
2018-04-17 14:41:33 -0400

    test

--- nScrapy/spiders/nScrapy.py
+++ nScrapy/spiders/nScrapy.py
@@ -4,18 +4,13 @@ Scrape Newegg URLs using a list of IDs
 """
 
 import scrapy
-import time
-#from time import sleep
 
 class nScrapy(scrapy.Spider):
 	name = 'nScrapy'
 	allowed_domains = ['newegg.com']
-#	with open("ids.txt", "rt") as f:
-#		start_urls = ["https://www.newegg.com/Product/Product.aspx?Item=" + url.strip() for url in f.readlines()]
 	start_urls = ["https://www.newegg.com/Product/Product.aspx?Item=050-013N-00001"]
 		
 	def parse(self, response):
-		time.sleep(10)
 		livePage = response.xpath('//script[contains(text(), "pstatus")]/text()').extract_first()
 		yield {
 			'Page ID': response.css('input#persMainItemNumber').xpath('@value').extract_first(),
